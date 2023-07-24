ab7081b69def696cfeb6a4ce2299389e6f4d1481
nomomax <nomomax@gmail.com>
2023-07-24 15:51:06 +0800

    match and destructure with patterns

--- lib/main.dart
+++ lib/main.dart
@@ -30,16 +30,16 @@ class DocumentScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final metadataRecord = document.metadata;
+    final (title, modified: modified) = document.metadata;
 
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text(metadataRecord.$1)),
+        title: Center(child: Text(title)),
       ),
       body: Column(
         children: [
           Center(
-            child: Text('Last modified ${metadataRecord.modified}'),
+            child: Text('Last modified $modified'),
           ),
         ],
       ),
